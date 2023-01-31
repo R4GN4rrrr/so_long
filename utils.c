897fb7470aa6e2e3dda2f201870ec1c9c7343db6
Youssef Menyouby <ymenyoub@e2r1p4.1337.ma>
2023-01-31 04:51:54 +0100

    push

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: ymenyoub <ymenyoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 04:02:41 by ymenyoub          #+#    #+#             */
-/*   Updated: 2023/01/31 04:35:07 by ymenyoub         ###   ########.fr       */
+/*   Updated: 2023/01/31 04:51:25 by ymenyoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 void	wall(t_map *map, int l, int k)
 {
 	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->wall, l, k);
-	mlx_string_put(map->mlx_ptr, map->win_ptr, 15, 15, 0xFFFFF0, ft_itoa(map->movement));
+	mlx_string_put(map->mlx_ptr, map->win_ptr,
+		15, 15, 0xFFFFF0, ft_itoa(map->movement));
 }
 
 void	coin(t_map *map, int l, int k)
 {
 	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->ground, l, k);
 	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->coin, l, k);
-	mlx_string_put(map->mlx_ptr, map->win_ptr, 15, 15, 0xFFFFF0, ft_itoa(map->movement));
+	mlx_string_put(map->mlx_ptr, map->win_ptr,
+		15, 15, 0xFFFFF0, ft_itoa(map->movement));
 }
 
 void	dooor(t_map *map, int l, int k)
@@ -31,18 +33,21 @@ void	dooor(t_map *map, int l, int k)
 		mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->o_door, l, k);
 	else
 		mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->c_door, l, k);
-	mlx_string_put(map->mlx_ptr, map->win_ptr, 15, 15, 0xFFFFF0, ft_itoa(map->movement));
+	mlx_string_put(map->mlx_ptr, map->win_ptr,
+		15, 15, 0xFFFFF0, ft_itoa(map->movement));
 }
 
 void	zero(t_map *map, int l, int k)
 {
 	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->ground, l, k);
-	mlx_string_put(map->mlx_ptr, map->win_ptr, 15, 15, 0xFFFFF0, ft_itoa(map->movement));
+	mlx_string_put(map->mlx_ptr, map->win_ptr,
+		15, 15, 0xFFFFF0, ft_itoa(map->movement));
 }
 
 void	player(t_map *map, int l, int k)
 {
 	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->ground, l, k);
 	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->player, l, k);
-	mlx_string_put(map->mlx_ptr, map->win_ptr, 15, 15, 0xFFFFF0, ft_itoa(map->movement));
+	mlx_string_put(map->mlx_ptr, map->win_ptr,
+		15, 15, 0xFFFFF0, ft_itoa(map->movement));
 }
